129dde0569a022c25630f275fb6c1cc083fd939f
Nate Watkins <natewatkinsca@gmail.com>
2023-10-06 11:31:55 -0700

    updated js to create/move rndm dots every ctrl+r

--- script.js
+++ script.js
@@ -1,4 +1,44 @@
-const canvas = document.getElementById('canvas')
-const ctx = canvas.getContent('2d')
+const canvas = document.getElementById('canvas1')
+const ctx = canvas.getContext('2d')
 canvas.width = window.innerWidth
 canvas.height = window.innerHeight
+
+//canvasd settings
+ctx.fillStyle = 'white'
+ctx.strokeStyle = 'white'
+ctx.lineWidth = 1
+
+class Particle {
+    constructor(effect) {
+        this.effect = effect
+        this.x = Math.floor(Math.random() * this.effect.width)
+        this.y = Math.floor(Math.random() * this.effect.height)
+    }
+    draw(context) {
+        context.fillRect(this.x, this.y, 10, 10)
+    }
+}
+
+class Effect {
+    constructor(width, height) {
+        this.width = width
+        this.height= height
+        this.particles = []
+        this.numOfParticles = 50
+    }
+    init() {
+        //creates particles 
+        for( let i = 0; i < this.numOfParticles; i++) {
+            this.particles.push(new Particle(this))
+        }
+    }
+    render(context) {
+        this.particles.forEach(particle => {
+            particle.draw(context)
+        })
+    }
+}
+
+const effect = new Effect(canvas.width, canvas.height)
+effect.init()
+effect.render(ctx)
