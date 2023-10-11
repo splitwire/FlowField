0a16aac2f5c9a72e525303b3769b20f6019659a5
Nate Watkins <natewatkinsca@gmail.com>
2023-10-11 13:48:34 -0700

    test

--- script.js
+++ script.js
@@ -13,9 +13,35 @@ class Particle {
         this.effect = effect
         this.x = Math.floor(Math.random() * this.effect.width)
         this.y = Math.floor(Math.random() * this.effect.height)
+        this.speedX = Math.random() * 5- 2.5
+        this.speedY = Math.random() * 5- 2.5
+        this.history = [{x: this.x, y: this.y}]
+        this.maxLength = Math.floor(Math.random() * 100 + 10)
+        this.angle = 0
     }
     draw(context) {
-        context.fillRect(this.x, this.y, 10, 10)
+        context.beginPath()
+        context.moveTo(this.history[0].x, this.history[0].y)
+        for (let i = 0; i < this.history.length; i++) {
+            context.lineTo(this.history[i].x, this.history[i].y)
+        }
+        context.stroke()
+    }
+    update() {
+        let x = Math.floor(this.x / this.effect.cellSize)
+        let y = Math.floor(this.y / this.effect.cellSize)
+        let index = y * this.effect.cols + x
+        this.angle = this.effect.flowField[index]
+
+        this.speedX = Math.cos(this.angle)
+        this.speedY = Math.sin(this.angle)
+        this.x += this.speedx
+        this.y += this.speedY
+
+        this.history.push({x: this.x, y: this.y})
+        if(this.history.length > this.maxLength) {
+            this.history.shift()
+        }
     }
 }
 
@@ -25,8 +51,25 @@ class Effect {
         this.height= height
         this.particles = []
         this.numOfParticles = 50
+        this.cellSize = 20
+        this.rows
+        this.cols
+        this.flowField =[]
+        this.init()
     }
     init() {
+        //create flow field
+        this.rows = Math.floor(this.height / this.cellSize)
+        this.cols = Math.floor(this.width / this.cellSize)
+        this.flowField = []
+        for(let y = 0; y < this.rows; y++) {
+            for(let x = 0; x < this.cols; x++) {
+                let angle = (Math.cos(x) + Math.sin(y)) * 2
+                this.flowField.push(angle)
+            }
+        }
+        console.log(this.flowField);
+
         //creates particles 
         for( let i = 0; i < this.numOfParticles; i++) {
             this.particles.push(new Particle(this))
@@ -35,10 +78,17 @@ class Effect {
     render(context) {
         this.particles.forEach(particle => {
             particle.draw(context)
+            particle.update()
         })
     }
 }
 
 const effect = new Effect(canvas.width, canvas.height)
-effect.init()
 effect.render(ctx)
+
+function animate() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height)
+    effect.render(ctx)
+    requestAnimationFrame(animate)
+}
+animate()
